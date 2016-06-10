8f23e0ef1302fcf44f62977efe03d4709cc9afe3
Coen <coen@compassmentis.com>
2016-06-10 15:36:36 +0100

    create_test_data to also create tables, if necessary

--- create_test_data.py
+++ create_test_data.py
@@ -1,4 +1,4 @@
-# Runs both locally (using python index.py) and on WebFaction using WSGI
+# Runs both locally (using python index.py) and on WebFaction
 
 import random
 import sys
@@ -6,23 +6,29 @@ import os
 
 from flask_sqlalchemy import SQLAlchemy
 
-from app import app
-
 CURRENT_FILE = os.path.abspath(__file__)
 CURRENT_DIR = os.path.dirname(CURRENT_FILE)
 PROJECT_DIR = os.path.dirname(CURRENT_DIR)
 
-# Add project top-dir to path (since it has no __init__.py)
-# sys.path.append(PROJECT_DIR + '/src/')
-
-# Add virtualenv to path - only for WebFaction, should be ignored locally
+# For WebFaction, should be ignored locally
 sys.path.append(PROJECT_DIR + '/virtualenv/lib/python3.5/site-packages/')
+sys.path.append(PROJECT_DIR + '/src/')
+sys.path.append(PROJECT_DIR + '/src/app/')
+
+from app import app
 
 db = SQLAlchemy(app)
 
 from app.models import Name, NameFrequency
 
 def create_test_data():
+
+    # Create the tables, if not done yet
+    try:
+        db.create_all()
+    except:
+        pass
+
     db.session.query(NameFrequency).delete()
     db.session.query(Name).delete()
     db.session.commit()
@@ -36,8 +42,6 @@ def create_test_data():
                 boys_count=random.randint(50, 100),
                 girls_count=random.randint(50, 100))
     db.session.commit()
-
-# Export the Flask app object from the project as wsgi application object
 
 if __name__ == '__main__':
     create_test_data()
